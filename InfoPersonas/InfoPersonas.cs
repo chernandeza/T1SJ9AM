c12faf61bb8e8b2f47bd14192c85b6c7eeb747f5
chernandeza <chernandez7@gmail.com>
2016-06-12 10:44:59 -0600

    Primer commit

---
+++ InfoPersonas/InfoPersonas.cs
@@ -0,0 +1,34 @@
+﻿using LibreriaPrincipal;
+using System;
+
+namespace InfoPersonas
+{
+    class InfoPersonas
+    {
+        static void Main(string[] args)
+        {
+            PersonaFisica perF = new PersonaFisica();
+            PersonaJuridica perJ = new PersonaJuridica();
+            perF.Identificacion = "1-1000-2000";
+            perF.Fecha = new DateTime(1975, 4, 20);
+            perF.Direccion = "San Jose";
+            perF.Nombre = "Fernando";
+            perF.Apellidos = "Torres Rojas";
+            perF.Sexo = Genero.Masculino;
+            perF.EstadoCivil = EstadoCivil.Casado;
+
+            perJ.Identificacion = "3-101-123456";
+            perJ.Fecha = new DateTime(2000, 6, 9);
+            perJ.Direccion = "Alajuela";
+            perJ.RazonSocial = "La empresa exitosa S.A";
+            perJ.Tipo = TipoPJ.SociedadAnonima;
+            perJ.Representante = perF;
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            perJ.DesplegarDatos();
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LibreriaPrincipal/PersonaFisica.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace LibreriaPrincipal
+{
+    public class PersonaFisica : Persona
+    {
+        private String _nombre;
+
+        public String Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        private String _apellidos;
+
+        public String Apellidos
+        {
+            get { return _apellidos; }
+            set { _apellidos = value; }
+        }
+
+        private Genero _sexo;
+
+        public Genero Sexo
+        {
+            get { return _sexo; }
+            set { _sexo = value; }
+        }
+
+        private EstadoCivil _estadoCivil;
+
+        public EstadoCivil EstadoCivil
+        {
+            get { return _estadoCivil; }
+            set { _estadoCivil = value; }
+        }
+
+        public override void DesplegarDatos()
+        {
+            Console.WriteLine("\t*** Informacion de Persona Fisica ***");
+            Console.WriteLine("\t\tIdentificacion: " + Identificacion);
+            Console.WriteLine("\t\tFecha de nacimiento: " + Fecha);
+            Console.WriteLine("\t\tDireccion: " + Direccion);
+            Console.WriteLine("\t\tNombre: " + Nombre + " " + Apellidos);
+            Console.WriteLine("\t\tSexo: " + Sexo.ToString());
+            Console.WriteLine("\t\tEstado Civil: " + EstadoCivil.ToString());    
+        }
+
+        public PersonaFisica()
+        {
+            Identificacion = "";
+            Direccion = "";
+            Fecha = DateTime.Now;
+            Nombre = "";
+            Apellidos = "";
+            Sexo = Genero.Ninguno;
+            EstadoCivil = EstadoCivil.Ninguno;
+        }
+    }
+
+    public enum Genero
+    {
+        Ninguno,
+        Masculino,
+        Femenino,
+        NoEspecifica
+    }
+
+    public enum EstadoCivil
+    {
+        Ninguno,
+        Soltero,
+        Casado,
+        Viudo,
+        Divorciado,
+        UnionLibre
+    }
+}
